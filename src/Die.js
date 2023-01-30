a8b156452b6638d85bdb6ebef8c08c282a4705cf
Derek <nightwing917@htomail.com>
2023-01-29 19:45:38 -0800

    forgot bind this

--- src/Die.js
+++ src/Die.js
@@ -5,7 +5,7 @@ class Die extends Component {
   constructor(props) {
     super(props);
 
-    
+    this.handleClick = this.handleClick.bind(this);
   }
   handleClick() {
      this.props.handleClick(this.props.idx);
